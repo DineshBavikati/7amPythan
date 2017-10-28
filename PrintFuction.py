218456b5c4eebce2d687db5c64e2037d0bc8bfdd
DineshBavikati <bktdine07@gmail.com>
2017-10-28 11:15:41 +0530

    myfirstCommit

---
+++ PrintFuction.py
@@ -0,0 +1,38 @@
+#!usr/bin/python
+'Simple Python program that demonstrate the print function'
+#single line comments ' ' " "
+#'single quotes ' ' can be used to comment sigle line  AND String value reprasention Name='DineshBavikati1' '
+#"double quotes " " can be used as single line comments AND String value representation EmpName="kiran""
+
+#Multi line comment
+#single triples and double triples are used for multi line comments and String value FirstName='''rajesh''' LastName="""chowdary"""
+
+''' hi
+here i am leraning about
+python printFuction 
+....'''
+""" 
+print fuction 
+with example """
+# lets start with print
+
+print(3+4)
+#if adding the string and int value it will give 
+
+#print(8+"Dinesh")
+#TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+#print("dinesh"+1)
+#TypeError: must be str, not int
+
+name = "ramesh"
+print( name )
+
+print(" ")  
+print('DineshBavikati')
+print(" ")
+print("compared to Java python language having lot of in built modules")
+print(" ")
+print(''' python basics are very "easy to learn ''')
+print(" ")
+print(""" here double triples can be used as both comment and a String Value""")
