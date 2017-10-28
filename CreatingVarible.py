1df280cf29d6b7e11b85399b67de76406cebccf3
DineshBavikati <bktdine07@gmail.com>
2017-10-28 12:32:20 +0530

    day4ClassPrograms

---
+++ CreatingVarible.py
@@ -0,0 +1,13 @@
+#!usr/bin/python
+
+CAN_VOLUME=0.355
+BOTTLE_VOLUME=2.0 
+CanPerPack=6
+
+totalVolume= CAN_VOLUME*CanPerPack
+
+print(totalVolume)
+
+totalVolume= totalVolume*BOTTLE_VOLUME
+
+print(totalVolume)
---
+++ StringVarible.py
@@ -0,0 +1,35 @@
+#!usr/bin/python
+
+#this program is String Example program
+
+#assign the string value to the variable
+FirstName="Guido"
+
+LastName='Van Rossum'
+
+print(FirstName,LastName)
+print(" ")
+print(FirstName+LastName)
+print(FirstName+ "and" + LastName)
+
+#String Indexing
+#String form left to right it will start from 0,1,...nth
+#String Right to Lest it will Start from -nth end with -1
+#Name =  "Dinesh Bavikati"
+#         0123456789.....
+#    -nth       987654321
+
+intional = FirstName[0]+ "&" + LastName[0]
+
+print(intional)
+print(FirstName[1])
+print(FirstName[-1])
+print(LastName[-10])
+
+#length is a function used to display the length of string
+print(len(FirstName))
+
+
+
+
+
